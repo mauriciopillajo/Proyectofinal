3f044cb9eff58fb4e07be98fb03781dc075e77e2
mpillajot <mpillajot@ucenfotec.ac.cr>
2023-04-24 22:54:05 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-
+app.use(express.static("public"));
 
 app.get("/", cors(), (req, res, next) =>{
     //console.log(req.body)
@@ -14,7 +14,7 @@ app.options('*', cors())
 var insertupdate = require('./routes/insertupdate');
 
 app.use(insertupdate);
-app.use(express.static("public"));
+
 
 
 app.post("/insertupdate",cors(),(req, res) =>{
